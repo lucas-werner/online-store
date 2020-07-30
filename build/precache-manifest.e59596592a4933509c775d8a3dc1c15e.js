642e278606781ed33f3b83f015f90c27c6f57292
Lucas Werner <lucas.1werner@gmail.com>
2020-07-30 12:37:47 -0300

    DEPLOY

---
+++ build/precache-manifest.e59596592a4933509c775d8a3dc1c15e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b42d2111314a5c85427d50be688f40",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "b7639642d12ef458b1e4",
+    "url": "/online-store/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "963fb179d3d0b15ead21",
+    "url": "/online-store/static/css/main.b06e0128.chunk.css"
+  },
+  {
+    "revision": "b7639642d12ef458b1e4",
+    "url": "/online-store/static/js/2.eb203ed0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/online-store/static/js/2.eb203ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963fb179d3d0b15ead21",
+    "url": "/online-store/static/js/main.024228cf.chunk.js"
+  },
+  {
+    "revision": "5e32c43b4a8c91ba7f6c",
+    "url": "/online-store/static/js/runtime-main.26de3b01.js"
+  },
+  {
+    "revision": "461429291bcbe67ab7e05315ef447275",
+    "url": "/online-store/static/media/cart.46142929.svg"
+  }
+]);
